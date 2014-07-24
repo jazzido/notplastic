78fe17e0c03a46f09307f2687c8a7dc2f61159c1
Manuel Aristaran <jazzido@jazzido.com>
2014-07-24 06:29:50 -0700

    manage

--- manage.py
+++ manage.py
@@ -1,16 +1,22 @@
+import os
 from flask import url_for
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
-from notplastic import create_app, db, DEV_CONFIG
+from notplastic import create_app, db, DEV_CONFIG, prod_config
 
-##### THIS IS FOR DEV ENVIRONMENT
-from notplastic.mp_creds import MP_CREDS
 
-DEV_CONFIG.update(MP_CREDS)
+conf = {}
+if not os.environ.get('PROD'):
+    ##### THIS IS FOR DEV ENVIRONMENT
+    from notplastic.mp_creds import MP_CREDS
+    conf = DEV_CONFIG
+    conf.update(MP_CREDS)
+else:
+    conf = prod_config.PROD_CONFIG
 
-app = create_app(**DEV_CONFIG)
-app.debug = True
+app = create_app(**conf)
+app.debug = not os.environ.get('PROD') is None
 
 migrate = Migrate(app, db)
 
