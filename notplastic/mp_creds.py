e8e50fb34cd3cef2bb67befeea0b7b46506ae6f3
Manuel Aristaran <jazzido@jazzido.com>
2017-02-14 17:33:38 -0500

    MP creds for dev

---
+++ notplastic/mp_creds.py
@@ -0,0 +1,7 @@
+# usuario prueba ML:
+# {"id":163048080,"nickname":"TETE4009168","password":"qatest4598","site_status":"active","email":"test_user_43412472@testuser.com"}
+# creds del usuario de prueba
+MP_CREDS = {
+    'MERCADOPAGO_CLIENT_ID': '828875117262459',
+    'MERCADOPAGO_CLIENT_SECRET': '64sr5Lb7kTQ5OSH4KSJ1kNKgcXzfpI96',
+}
